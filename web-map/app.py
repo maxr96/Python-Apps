65e2deff9684b891e62a07b8c8e2da5def1b0722
Maksym Radchuk <maxr96@gmail.com>
2021-03-02 03:05:42 +0100

    Add population colouring and layering support for web map app

--- web-map/app.py
+++ web-map/app.py
@@ -1,8 +1,7 @@
 import folium
 import pandas
-map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
-fg = folium.FeatureGroup(name="My Map")
-data = pandas.read_csv("Volcanoes.txt")
+
+data = pandas.read_csv("web-map/Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
 elev = list(data["ELEV"])
@@ -14,11 +13,22 @@ def color_producer(elevation):
         return 'orange'
     else:
         return 'red'
-        
-print(data)
+
+map = folium.Map(location=[38.58, -99.09], zoom_start=6, tiles="Stamen Terrain")
+fgv = folium.FeatureGroup(name="Volcanoes")
+
 for lt, ln, el in zip(lat, lon, elev):
-    fg.add_child(folium.CircleMarker(location=[lt,ln], radius=6, popup=el, 
+    fgv.add_child(folium.CircleMarker(location=[lt,ln], radius=6, popup=el, 
     fill_color=color_producer(el), color='grey', fill_opacity=0.7))
 
-map.add_child(fg)
-map.save("Map1.html")
+fgp = folium.FeatureGroup(name="Population")
+fgp.add_child(folium.GeoJson(data=open('web-map/world.json','r', encoding='utf-8-sig').read(), 
+style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005'] < 1000000 
+else 'orange' if 1000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
+
+map.save("web-map/Map1.html")
